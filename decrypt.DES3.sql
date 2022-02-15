cd9d52565c056cc29201dc255916b8210948a52a
kamleong <kamleong@users.noreply.github.com>
2022-02-15 09:12:37 +0800

    Update decrypt.DES3.sql

--- decrypt.DES3.sql
+++ decrypt.DES3.sql
@@ -1,5 +1,22 @@
 DECLARE
 
+  FUNCTION encryptDES3 (
+    piv_str VARCHAR2, piv_strKey VARCHAR2
+  ) RETURN VARCHAR2 IS
+    lr_encrypted_raw  RAW(2048);
+  BEGIN
+    lr_encrypted_raw := DBMS_CRYPTO.ENCRYPT(
+      src => UTL_RAW.CAST_TO_RAW(piv_str), -- UTL_I18N.STRING_TO_RAW (string, 'AL32UTF8');
+      typ => DBMS_CRYPTO.DES3_CBC_PKCS5,
+      key => UTL_RAW.CAST_TO_RAW(piv_strKey)
+    );
+    RETURN RawToHex(lr_encrypted_raw);
+  EXCEPTION
+    WHEN OTHERS THEN
+      Dbms_Output.put_line('ERROR in encryptDES3():' || SQLERRM);
+      RETURN NULL;
+  END;
+
   FUNCTION decryptDES3 (
     piv_strHex VARCHAR2, piv_strKey VARCHAR2
   ) RETURN VARCHAR2 IS

6a5e860a3a7c9c022ac749d6e7df2545184007cb
kamleong <kamleong@users.noreply.github.com>
2024-03-09 15:26:56 +0800

    Update decrypt.DES3.py

--- decrypt.DES3.py
+++ decrypt.DES3.py
@@ -1,7 +1,9 @@
-from Crypto.Cipher import DES3
+from Crypto.Cipher import DES3 ## pycryptodome | pycrypto
+from Crypto.Util.Padding import unpad
 def decryptDES3(p_data, p_key):
-  cipher = DES3.new(bytes(p_key[:24], encoding='ascii'), DES3.MODE_CBC, '\0'*8)
-  decryptData = cipher.decrypt( bytes.fromhex(p_data) )
-  decryptData = str(decryptData, encoding='ascii', errors='ignore')
+  cipher = DES3.new(p_key[:24].encode('ascii'), DES3.MODE_CBC, bytes('\0'*8,encoding='ascii'))
+  decryptData = cipher.decrypt(bytes.fromhex(p_data))
+  decryptData = unpad(decryptData,8) ## PaddingMode.PKCS7
+  decryptData = decryptData.decode("ascii", errors='ignore') ## str(decryptData, encoding='ascii', errors='ignore')
   return decryptData
 #
